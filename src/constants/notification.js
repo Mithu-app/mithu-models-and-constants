32a295700651a117ddcd1a3899ce1d1b4201b033
dawar-latif <dawar@mithu.com>
2024-10-09 17:54:21 +0500

    notifications contsant updated

--- src/constants/notification.js
+++ src/constants/notification.js
@@ -9,7 +9,30 @@ const NOTIFICATION_TYPES = {
     GLOBAL: "global"
 }
 
+const NOTIFICATION_DESCRIPTION_ARABIC = {
+  MEMBERSHIP_CARD_MINTING_CREATION: (merchantName) =>
+    ` تم إنشاء بطاقة عضوية NFT الخاصة بـ ${merchantName}`,
+  MEMBERSHIP_CARD_RESHCEDULING: (merchantName, scheduledTime) =>
+    ` تمت إعادة جدولة بطاقة عضوية NFT الخاصة بك من ${merchantName} إلى ${scheduledTime}`,
+  MEMBERSHIP_CARD_PROGRESS: (merchantName) =>
+    ` بطاقة عضويتك الخاصة بـ ${merchantName} قيد التقدم`,
+  MEMBERSHIP_CARD_SCHEDULING: (merchantName, scheduledTime) =>
+    ` تمت جدولة بطاقة عضوية NFT الخاصة بك من ${merchantName} إلى ${scheduledTime}`,
+};
+const NOTIFICATION_TITLE_ARABIC = {
+  MEMBERSHIP_CARD_MINTING: "سك بطاقة العضوية",
+};
+
+const NOTIFICATION_DESCRIPTION_ENGLISH = {};
+
+const NOTIFICATION_TITLE_ENGLISH = {
+  MEMBERSHIP_CARD_MINTING: "membership card minting",
+};
 module.exports = {
-    NOTIFICATION_STATUS,
-    NOTIFICATION_TYPES
-}
+  NOTIFICATION_DESCRIPTION_ARABIC,
+  NOTIFICATION_TITLE_ARABIC,
+  NOTIFICATION_DESCRIPTION_ENGLISH,
+  NOTIFICATION_TITLE_ENGLISH,
+  NOTIFICATION_STATUS,
+  NOTIFICATION_TYPES,
+};
