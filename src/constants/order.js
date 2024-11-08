67ba501c89c6d33ef3b767d973260282131fdd23
shamil@mithu.app <shamil@mithu.app>
2024-11-08 16:22:27 +0500

    chore: order platform types

--- src/constants/order.js
+++ src/constants/order.js
@@ -3,6 +3,8 @@ const ORDER_PLATFORM_TYPE = {
   FOODICS: "foodics",
   LAZYWAIT: "lazywait",
   VENDOR_NON_INTEGRATED: "vendor_non_integrated",
+  MITHU_CASHIER_APP : "mithu_cashier_app"
+
 };
 
 module.exports = {
