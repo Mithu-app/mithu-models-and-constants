9d5fbd2a2c3cb0fbfdc87c5f0eb1de73829f7db9
naveed <naveed@mithu.com>
2024-11-08 12:20:40 +0500

    quick fix: platform type

--- src/constants/order.js
+++ src/constants/order.js
@@ -1,8 +1,9 @@
 const ORDER_PLATFORM_TYPE = {
-    MITHU: 'mithu',
-    FOODICS: 'foodics',
-    LAZYWAIT: 'lazywait'
-}
+  MITHU: "mithu",
+  FOODICS: "foodics",
+  LAZYWAIT: "lazywait",
+  VENDOR_NON_INTEGRATED: "vendor_non_integrated",
+};
 
 module.exports = {
     ORDER_PLATFORM_TYPE
