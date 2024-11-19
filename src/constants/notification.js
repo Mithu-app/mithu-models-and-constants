7d6daf48c21aa24f23c8fcdfed271cf294c59c50
shamil@mithu.app <shamil@mithu.app>
2024-11-19 13:52:49 +0500

    add mithu as collection

--- src/constants/notification.js
+++ src/constants/notification.js
@@ -1,16 +1,16 @@
 const NOTIFICATION_STATUS = {
-    READ: "read",
-    UNREAD: "unread"
+  READ: "read",
+  UNREAD: "unread"
 }
 
 const NOTIFICATION_TYPES = {
-    INDIVIDUAL: "individual",
-    CUSTOM: "custom",
-    GLOBAL: "global"
+  INDIVIDUAL: "individual",
+  CUSTOM: "custom",
+  GLOBAL: "global"
 }
 
 const NOTIFICATION_DESCRIPTION_ARABIC = {
-    
+
   ORDER_THROUGH_MERCHANT: "شكرا لك على الطلب مع ميثو.",
   MEMBERSHIP_CARD_MINTING_CREATION: (merchantName) =>
     ` تم إنشاء بطاقة عضوية NFT الخاصة بـ ${merchantName}`,
@@ -20,20 +20,24 @@ const NOTIFICATION_DESCRIPTION_ARABIC = {
     ` بطاقة عضويتك الخاصة بـ ${merchantName} قيد التقدم`,
   MEMBERSHIP_CARD_SCHEDULING: (merchantName, scheduledTime) =>
     ` تمت جدولة بطاقة عضوية NFT الخاصة بك من ${merchantName} إلى ${scheduledTime}`,
+  PURCHASE_THROUGH_STORE: "شكرا لك على الشراء من المتجر مع ميثو.",
 };
 const NOTIFICATION_TITLE_ARABIC = {
   MEMBERSHIP_CARD_MINTING: "سك بطاقة العضوية",
   ORDER_THROUGH_MERCHANT: "الشراء من خلال التاجر",
+  PURCHASE_THROUGH_STORE: "شكرا لك على الشراء من المتجر مع ميثو.",
 };
 
 const NOTIFICATION_DESCRIPTION_ENGLISH = {
-    
-  ORDER_THROUGH_MERCHANT: "Thank you for ordering with Mithu.",
+
+  ORDER_THROUGH_MERCHANT: "thank you for ordering with Mithu.",
+  PURCHASE_THROUGH_STORE: "thank you for ordering with Mithu.",
 };
 
 const NOTIFICATION_TITLE_ENGLISH = {
   MEMBERSHIP_CARD_MINTING: "membership card minting",
-  ORDER_THROUGH_MERCHANT: "Purchase through merchant",
+  ORDER_THROUGH_MERCHANT: "purchase through merchant",
+  PURCHASE_THROUGH_STORE: "purchase through store",
 };
 module.exports = {
   NOTIFICATION_DESCRIPTION_ARABIC,
