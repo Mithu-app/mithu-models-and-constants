762598ab1b0be8b93d559dbc4fd6db66f3066acd
naveed <naveed@mithu.com>
2025-01-14 17:48:31 +0500

    quick fix: manual receipt model

--- src/models/ManualReceipt.js
+++ src/models/ManualReceipt.js
@@ -1,59 +1,120 @@
 "use strict";
 
 const { Schema, model } = require("mongoose");
-const { COLLECTION, TIMESTAMPS, MODEL: NAME, MANUAL_RECEIPT_STATUS, MANUAL_RECEIPT_TYPE } = require("../constants");
-
+const {
+  COLLECTION,
+  TIMESTAMPS,
+  MODEL: NAME,
+  MANUAL_RECEIPT_STATUS,
+  MANUAL_RECEIPT_TYPE,
+} = require("../constants");
 
 const SCHEMA = new Schema(
-    {
-        merchant_id: {
-            type: Schema.Types.ObjectId,
-            ref: NAME.LANGUAGE,
-            required: true
-        },
-        image: {
-            type: String,
-            required: true
-        },
-        date: {
-            type: String,
-            required: true
-        },
-        customer_id: {
-            type: Schema.Types.ObjectId,
-            ref: NAME.CUSTOMER,
-            required: true
-        },
-        status: {
-            type: String,
-            enum: Object.values(MANUAL_RECEIPT_STATUS),
-            default: MANUAL_RECEIPT_STATUS.PENDING,
-            required: true
-        },
-        order_amount: {
-            type: Number
-        },
-        reason: {
-            type: String
-        },
-        updated_by: {
-            type: Schema.Types.ObjectId,
-            ref: NAME.USER
-        },
-        deleted_at: {
-            type: Date
-        },
-        deleted_by: {
-            type: Schema.Types.ObjectId,
-            ref: NAME.USER
-        }
+  {
+    merchant_id: {
+      type: Schema.Types.ObjectId,
+      ref: NAME.MERCHANT,
+      required: true,
     },
-    {
-        collection: COLLECTION.MANUAL_RECEIPT,
-        timestamps: TIMESTAMPS,
-    }
+    image: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    customer_id: {
+      type: Schema.Types.ObjectId,
+      ref: NAME.CUSTOMER,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: Object.values(MANUAL_RECEIPT_STATUS),
+      default: MANUAL_RECEIPT_STATUS.PENDING,
+      required: true,
+    },
+    order_amount: {
+      type: Number,
+    },
+    reason: {
+      type: String,
+    },
+    created_by: {
+      type: Schema.Types.ObjectId,
+      ref: NAME.USER,
+    },
+    updated_by: {
+      type: Schema.Types.ObjectId,
+      ref: NAME.USER,
+    },
+    deleted_at: {
+      type: Date,
+    },
+    deleted_by: {
+      type: Schema.Types.ObjectId,
+      ref: NAME.USER,
+    },
+  },
+  {
+    collection: COLLECTION.MANUAL_RECEIPT,
+    timestamps: TIMESTAMPS,
+  }
 );
+
+SCHEMA.static({
+  serialize(manualReceipt) {
+    const {
+      _id,
+      merchant_id,
+      image,
+      date,
+      customer_id,
+      status,
+      order_amount,
+      reason,
+      created_by,
+    } = manualReceipt;
+    return {
+      id: _id,
+      merchant_id,
+      image,
+      date,
+      customer_id,
+      status,
+      order_amount,
+      reason,
+      created_by,
+    };
+  },
+  getSelectableFields() {
+    return [
+      "_id",
+      "merchant_id",
+      "image",
+      "date",
+      "customer_id",
+      "status",
+      "order_amount",
+      "reason",
+      "created_by",
+    ];
+  },
+});
+
+SCHEMA.method({
+  serialize() {
+    return this.constructor.serialize(this);
+  },
+});
+
+SCHEMA.set("toJSON", {
+  transform(doc) {
+    return doc.serialize();
+  },
+});
 
 const MODEL = model(NAME.MANUAL_RECEIPT, SCHEMA);
 
-module.exports = MODEL
+module.exports = MODEL;
