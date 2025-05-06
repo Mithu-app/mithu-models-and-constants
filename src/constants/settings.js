5f6c77a00b5c064beb97012d10f7d6dd343aedc2
shamil@mithu.app <shamil@mithu.app>
2025-05-06 17:56:29 +0500

    add settings

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -9,7 +9,8 @@ const SETTINGS_KEYS = {
   EARNING_MESSAGE_TAQNYAT: "earning_message_taqnyat",
   IN_ACTIVE_VENDOR_DEVICES_ALERT_EMAILS: "in_active_vendor_devices_alert_emails",
   SIGNUP_TOPUP_POINTS: "signup_topup_points",
-  SIGNUP_TOPUP_TO_USERS: "signup_topup_to_users"
+  SIGNUP_TOPUP_TO_USERS: "signup_topup_to_users",
+  STOP_WHATSAPP_OTP_SERVICE: "stop_whatsapp_otp_service" 
 };
 
 module.exports = {
--- src/models/Slider.js
+++ src/models/Slider.js
@@ -25,7 +25,6 @@ const SCHEMA = new Schema(
         },
         entity_type: {
             type: String, 
-            enum: Object.values(NAME),
             default: null
         },
         status: {
